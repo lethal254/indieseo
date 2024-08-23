f0547c5b8cc8819cd1d32309a56f01a8938b0778
Benard Ogutu <benardogutu65@gmail.com>
2024-08-23 14:35:41 +0300

    Done

--- src/middlewares/passportConfig.ts
+++ src/middlewares/passportConfig.ts
@@ -10,7 +10,7 @@ passport.use(
     {
       clientID: GOOGLE_CLIENT_ID,
       clientSecret: GOOGLE_CLIENT_SECRET,
-      callbackURL: "/auth/google/callback",
+      callbackURL: "https://indieseo.onrender.com/auth/google/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
       const user = await prisma.user.upsert({
